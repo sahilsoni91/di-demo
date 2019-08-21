6125a15e3cdfeafdac3c7c30b70b0e77e7bf34e4
Sahil Soni <E01602@hsc.com>
2019-08-21 18:23:08 +0530

    Spring context example

--- src/main/java/com/springdemo/didemo/DiDemoApplication.java
+++ src/main/java/com/springdemo/didemo/DiDemoApplication.java
@@ -2,12 +2,17 @@ package com.springdemo.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.springdemo.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController controller = (MyController)ctx.getBean("myController");
+		controller.hello();
 	}
 
 }
---
+++ src/main/java/com/springdemo/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.springdemo.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+	
+	public String hello() {
+		System.out.println("Hello");
+		return "test";
+	}
+}
