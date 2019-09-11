11bcaf888dcdc1af1c8f78bfecd5819848a66f3d
Sahil Soni <E01602@hsc.com>
2019-09-11 13:07:30 +0530

    Best Practice for command line application

--- src/main/java/com/springdemo/didemo/DiDemoApplication.java
+++ src/main/java/com/springdemo/didemo/DiDemoApplication.java
@@ -1,5 +1,7 @@
 package com.springdemo.didemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,17 +11,39 @@ import com.springdemo.didemo.controllers.MyController;
 import com.springdemo.didemo.controllers.PropertyInjectedController;
 import com.springdemo.didemo.controllers.SetterInjectedController;
 
+/*
+ * Implements CommandLineRunner interface to run as a console application
+ */
 @SpringBootApplication
-public class DiDemoApplication {
+public class DiDemoApplication implements CommandLineRunner{
+
+	//	public static void main(String[] args) {
+	//		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+	//		MyController controller = (MyController)ctx.getBean("myController");
+	//		
+	//		System.out.println(controller.hello());
+	//		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+	//		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+	//		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+	//		
+	//	}
+
+	@Autowired
+	ApplicationContext ctx;
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
 		MyController controller = (MyController)ctx.getBean("myController");
-		
+
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
+
 
 }
