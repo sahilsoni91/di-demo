f1b2c0ed3b0beabb741906552de1f0279d851e2e
Sahil Soni <E01602@hsc.com>
2020-01-15 13:31:07 +0530

    Environment properties reading

--- src/main/java/com/springdemo/didemo/config/PropertyConfig.java
+++ src/main/java/com/springdemo/didemo/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package com.springdemo.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.springdemo.didemo.datasourcebean.FakeDataSource;
 
@@ -12,6 +14,12 @@ import com.springdemo.didemo.datasourcebean.FakeDataSource;
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+	/*
+	 * GetProperties from environment variables
+	 */
+	@Autowired
+	Environment env;
+	
 	@Value("${demo.user}")
 	String username;
 	
@@ -33,9 +41,9 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource getFakeDataSource() {
 		FakeDataSource fDS = new FakeDataSource();
-		fDS.setUrl(url);
-		fDS.setUser(username);
-		fDS.setPassword(password);
+		fDS.setUrl(env.getProperty("DBURL"));
+		fDS.setUser(env.getProperty("USER"));
+		fDS.setPassword(env.getProperty("PWD"));
 		return fDS;
 	}
 }
