1158ec87e886bd0366e82f5173b5df62960f7847
Sahil Soni <E01602@hsc.com>
2020-01-15 15:52:08 +0530

    Reading and loading custom properties from spring boot default properties file

--- src/main/java/com/springdemo/didemo/config/PropertyConfig.java
+++ src/main/java/com/springdemo/didemo/config/PropertyConfig.java
@@ -1,28 +1,15 @@
 package com.springdemo.didemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import com.springdemo.didemo.datasourcebean.FakeDataSource;
 import com.springdemo.didemo.jmsbean.FakeJmsBroker;
 
 @Configuration
-//@PropertySource("classpath:datasource.properties")
-@PropertySources({@PropertySource("classpath:datasource.properties"), @PropertySource("classpath:jms.properties")})
 public class PropertyConfig {
 
-	/*
-	 * GetProperties from environment variables
-	 */
-	@Autowired
-	Environment env;
-	
 	@Value("${demo.user}")
 	String username;
 	
@@ -41,21 +28,12 @@ public class PropertyConfig {
     @Value("${demo.jms.jmsurl}")
     String jmsUrl;
 	
-	/*
-	 * Created this bean to read properties file mentioned in PropertySource annotation
-	 */
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyFileReader() {
-		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		return pspc;
-	}
-	
 	@Bean
 	public FakeDataSource getFakeDataSource() {
 		FakeDataSource fDS = new FakeDataSource();
-		fDS.setUrl(env.getProperty("DBURL"));
-		fDS.setUser(env.getProperty("USER"));
-		fDS.setPassword(env.getProperty("PWD"));
+		fDS.setUrl(url);
+		fDS.setUser(username);
+		fDS.setPassword(password);
 		return fDS;
 	}
 	
